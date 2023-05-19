ca6409dcd3256a01830f3bae12055039acedd68a
Ana Beatriz Marques Moreira <116691989+ana-bmm0623@users.noreply.github.com>
2023-05-18 22:22:05 -0300

    Update Principal.java

--- calculoDeAspercao/src/aplicacao/Principal.java
+++ calculoDeAspercao/src/aplicacao/Principal.java
@@ -4,7 +4,7 @@ import modelo.Cultura;
 
 public class Principal {
 	public static void main(String[] args) {
-		Cultura c = new Cultura(0.35, 0.18, 450.0, 0.5, 7.0, 0.8, 10.02, 20.0, 323, 3.66, 18.0, 13.0);
+		Cultura c = new Cultura();
 //		c.lerDados();
 		c.imprimir();
 		
